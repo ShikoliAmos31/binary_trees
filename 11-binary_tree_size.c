e18164ac10f14c221590ec3f527b98610128a22e
shikoliamos31 <amosshikoli@gmail.com>
2024-02-28 22:09:25 -0800

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,17 +4,12 @@
  * binary_tree_size - Measures the size of a binary tree
  * @tree: Pointer to the root node of the tree to measure
  *
- * Return: Size of the tree, 0 if tree is NULL
+ * Return: Size of the tree, or 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	/* Recursively calculate size of left and right subtrees */
-	size_t left_size = binary_tree_size(tree->left);
-	size_t right_size = binary_tree_size(tree->right);
-	/* Size of tree is the sum of sizes of left and right */
-	/* subtrees plus 1 (for the current node) */
-	return (left_size + right_size + 1);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
