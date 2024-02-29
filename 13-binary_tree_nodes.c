fc77b19c7e6127c0842afa47a2002fc461882ba2
shikoliamos31 <amosshikoli@gmail.com>
2024-02-28 22:20:10 -0800

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	size_t count = 0;
 
-	if (tree->left || tree->right)
+	if (tree->left != NULL || tree->right != NULL)
 		count++;
 
 	count += binary_tree_nodes(tree->left);
